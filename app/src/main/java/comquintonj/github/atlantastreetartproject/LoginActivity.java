a1e690fdc7de84d03773484873a9efd55651ab2a
quinton <qjohnson@gatech.edu>
2017-01-22 21:55:52 -0500

    Fixed login screen bug

--- app/src/main/java/comquintonj/github/atlantastreetartproject/LoginActivity.java
+++ app/src/main/java/comquintonj/github/atlantastreetartproject/LoginActivity.java
@@ -1,13 +1,177 @@
 package comquintonj.github.atlantastreetartproject;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MyActivity";
+    private Intent discoverIntent;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText emailText;
+    private EditText passwordText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+
+        // Views
+        emailText = (EditText) findViewById(R.id.loginUsername);
+        passwordText = (EditText) findViewById(R.id.loginPassword);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        // Login Button
+        discoverIntent = new Intent(this, DiscoverActivity.class);
+        Button loginBut = (Button) findViewById(R.id.loginButton);
+        loginBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn(emailText.getText().toString(), passwordText.getText().toString());
+
+            }
+        });
+
+        // Allow text view for guest to send user to discover screen
+        TextView guestView = (TextView) findViewById(R.id.guestTextView);
+        final Intent guestIntent = new Intent(this, DiscoverActivity.class);
+        guestView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInAnonymously();
+            }
+        });
+
+        // Allow text view to send user to registration screen
+        TextView registrationTextView = (TextView) findViewById(R.id.registerView);
+        final Intent registrationIntent = new Intent(this, RegistrationActivity.class);
+        registrationTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(registrationIntent);
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+    }
+
+    private void signInAnonymously() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInAnonymously", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailText.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailText.setError("Required.");
+            valid = false;
+        } else {
+            emailText.setError(null);
+        }
+
+        String password = passwordText.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordText.setError("Required.");
+            valid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        return valid;
     }
 }
+
+
+
---
+++ app/src/main/java/comquintonj/github/atlantastreetartproject/RegistrationActivity.java
@@ -0,0 +1,146 @@
+package comquintonj.github.atlantastreetartproject;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    // Instance variables
+    private static final String TAG = "MyActivity";
+    private EditText emailInfo;
+    private EditText passwordInfo;
+    private EditText confirmInfo;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        mAuth = FirebaseAuth.getInstance();
+
+        // Find views by ID for the text fields
+        emailInfo = (EditText) findViewById(R.id.emailInput);
+        passwordInfo = (EditText ) findViewById(R.id.passwordInput);
+        confirmInfo = (EditText ) findViewById(R.id.confirmInput);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        // Set up intent to go to login screen upon successful registration
+        final Intent loginIntent = new Intent(this,LoginActivity.class);
+
+        // Set up registration button to add User with the edited text fields
+        Button registerButton = (Button) findViewById(R.id.registrationButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailText = emailInfo.getText().toString();
+                String passwordText = confirmInfo.getText().toString();
+                String confirmText = passwordInfo.getText().toString();
+                if (!passwordText.equals(confirmText)) {
+                    Toast.makeText(RegistrationActivity.this, "Passwords do not match.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                createAccount(emailInfo.getText().toString(),
+                        confirmInfo.getText().toString());
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG, "onComplete: Failed=" + task.getException().getMessage());
+                            Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                });
+
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailInfo.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailInfo.setError("Required.");
+            valid = false;
+        } else {
+            emailInfo.setError(null);
+        }
+
+        String password = passwordInfo.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordInfo.setError("Required.");
+            valid = false;
+        } else {
+            passwordInfo.setError(null);
+        }
+
+        return valid;
+    }
+
+
+}

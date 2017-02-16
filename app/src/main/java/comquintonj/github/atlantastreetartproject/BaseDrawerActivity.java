0dd8ecea30fee5704e8e0d0ff86d1114b0768ef6
quinton <qjohnson@gatech.edu>
2017-02-16 00:31:18 -0500

    Navigation Drawer added to app, submit screen cleaned up, bug fixes, general UI cleanup

--- app/src/main/java/comquintonj/github/atlantastreetartproject/BaseDrawerActivity.java
+++ app/src/main/java/comquintonj/github/atlantastreetartproject/BaseDrawerActivity.java
@@ -3,7 +3,6 @@ package comquintonj.github.atlantastreetartproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
--- app/src/main/java/comquintonj/github/atlantastreetartproject/SubmitActivity.java
+++ app/src/main/java/comquintonj/github/atlantastreetartproject/SubmitActivity.java
@@ -1,14 +1,14 @@
 package comquintonj.github.atlantastreetartproject;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
@@ -16,13 +16,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.content.Intent;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
